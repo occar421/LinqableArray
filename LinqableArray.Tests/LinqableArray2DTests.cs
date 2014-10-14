9a63e572b28ab6049ef8948165889667f9377f5e
MasuqaT <occar@hotmail.co.jp>
2014-10-14 14:10:55 +0900

    Add LinqableArray2D test class file

---
+++ LinqableArray.Tests/LinqableArray2DTests.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using System.Linq;
+using MasuqatNet.Collections;
+
+namespace LinqableArray.Tests
+{
+	[TestClass]
+	public class LinqableArray2DTests
+	{
+	}
+}
