932e33309b57368b402a5c8fb6a1b8f3e7202b0c
MasuqaT <occar@hotmail.co.jp>
2014-10-14 13:44:41 +0900

    Make LinqableArray2D class base code

---
+++ LinqableArray/LinqableArray2D.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MasuqatNet.Collections
+{
+	public class LinqableArray2D<T>
+	{
+		private T[] _items;
+		private int _length1;
+		private int _length2;
+
+		public LinqableArray2D(int length1, int length2)
+		{
+			_length1 = length1;
+			_length2 = length2;
+			_items = new T[length1 * length2];
+		}
+
+		public T this[int index1, int index2]
+		{
+			get
+			{
+				if (index1 < 0 || _length1 <= index1)
+				{
+					throw new IndexOutOfRangeException("index1");
+				}
+				if (index2 < 0 || _length2 <= index2)
+				{
+					throw new IndexOutOfRangeException("index2");
+				}
+
+				var realIndex = index1 * _length2 + index2;
+				return _items[realIndex];
+			}
+			set
+			{
+				if (index1 < 0 || _length1 <= index1)
+				{
+					throw new IndexOutOfRangeException("index1");
+				}
+				if (index2 < 0 || _length2 <= index2)
+				{
+					throw new IndexOutOfRangeException("index2");
+				}
+
+				var realIndex = index1 * _length2 + index2;
+				_items[realIndex] = value;
+			}
+		}
+	}
+}
